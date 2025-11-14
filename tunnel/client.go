a8076ee2b74c55f6c498a99195780b44802cce45
bugfan <908958194@qq.com>
2025-11-14 15:34:52 +0800

    tunnel go demo ok

--- tunnel/client.go
+++ tunnel/client.go
@@ -3,13 +3,19 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/bugfan/i996-android/tunnel/client"
 )
 
 func main() {
-	tlsConfig := client.LoadTLSConfigFromBytes([]byte("cert/crrt.pem"))
+	certPEM, err := os.ReadFile("cert/cert.pem")
+	if err != nil {
+		fmt.Println("failed to read cert.pem:", err)
+		return
+	}
+	tlsConfig := client.LoadTLSConfigFromBytes(certPEM)
 	if tlsConfig != nil {
 		tlsConfig.ClientSessionCache = tls.NewLRUClientSessionCache(5000)
 	}
